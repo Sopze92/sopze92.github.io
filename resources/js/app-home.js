f04d7594b711b167a691e1f93b88615def5d0dd4
Sopze <60981650+Sopze92@users.noreply.github.com>
2024-02-09 05:30:11 +0100

    js scroller, minor html/css tweaks, mobile fixes

--- resources/js/app-home.js
+++ resources/js/app-home.js
@@ -1,20 +1,21 @@
-{
-  const deviceDesktop= checkDeviceDesktop();
-  
+!function(){
   if(deviceDesktop){
 
     const titlebg = document.getElementById("home-title-3D");
     const title = document.getElementById("home-title-bg").children[0];
-    var angx= .0, angy= .0, movx= .0, movy= .0, scale= 1.0;
+    var angx= .0, angy= .0, movx= .0, movy= .0;
   
     function transformElements(x, y) {
       let box = titlebg.getBoundingClientRect();
   
-      const musx= x - box.x - box.width * .5;
-      const musy= y - box.y - box.height * .5;
+      let halfBoxWidth= box.width * .5;
+      let halfBoxHeight= box.height * .5;
+
+      const musx= x - box.x - halfBoxWidth;
+      const musy= y - box.y - halfBoxHeight;
       const pxfactor= 100.0 / box.width * 200;
   
-      angx = musy * .0025;
+      angx = musy * .0045;
       angy = musx * .0025;
       movx = musx * -.0125;
       movy = musy * -.0125;
@@ -23,23 +24,12 @@
       title.style.transform = `perspective(200px) rotateX(${angx}deg) rotateY(${angy}deg) translate(${-movx*.75}px, ${-movy*.25}px)`;
     }
   
+    let u= true;
     document.addEventListener("mousemove", (e) => {
-      window.requestAnimationFrame(function () {
-          transformElements(e.clientX, e.clientY);
-      });
+      if(u=!u) window.requestAnimationFrame(function () { transformElements(e.clientX, e.clientY); });
     });
-  
-    Number.prototype.clamp = function(min, max) {
-      return Math.min(Math.max(this, min), max);
-    };
+    
+    Number.prototype.clamp = function(min, max) { return Math.min(Math.max(this, min), max); };
   }
+}();
 
-  function checkDeviceDesktop(){
-    const includes= ['win64', 'win32', 'linux', 'unix', 'mac', 'macos', 'mac os'];
-    const excludes= ['android', 'ios', 'mobile', 'tablet'];
-    const useragent= navigator.userAgent.toLowerCase();
-    return !excludes.some((e) => useragent.includes(e)) && includes.some((e) => useragent.includes(e));
-  }
-}
-
-window.onresize= function () { console.log('width:' + window.innerWidth); };
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,23 @@
+const deviceDesktop= checkDeviceDesktop();
+
+function checkDeviceDesktop(){
+  const includes= ['win64', 'win32', 'linux', 'unix', 'mac', 'macos', 'mac os'];
+  const excludes= ['android', 'ios', 'mobile', 'tablet'];
+  const useragent= navigator.userAgent.toLowerCase();
+  return !excludes.some((e) => useragent.includes(e)) && includes.some((e) => useragent.includes(e));
+}
+
 !function(){
 
-  const TAB_PATH_STR= "./resources/html/tab-%0.html";
+  const TAB_PATH_STR= "./resources/html/%0.html";
 
   const TAB_CONTAINER= document.getElementById("tab-container");
   const TAB_BUTTONS= [];
   const DEFAULT_TAB= 0;
   
+  var WORDS_PANNERS= [];
+  var bANY_PANNER= false;
+
   {
     const lButtons= document.getElementsByTagName("button");
     let p;
@@ -25,8 +37,14 @@
         for(const c of (b.includes)) c.remove(); 
         b.includes= undefined;
       }
-      if( b.element === e ) e.classList.add("active"); 
-      else b.element.classList.remove("active");
+      if( b.element === e ) {
+        e.classList.add("active"); 
+        b.element.setAttribute("aria-current", "page");
+      }
+      else {
+        b.element.classList.remove("active");
+        b.element.removeAttribute("aria-current");
+      }
     }
     loadPage(e.getAttribute("tab"));
   }
@@ -46,8 +64,6 @@
   }
 
   function onPageLoaded(htmlobj){
-    
-    //console.log(`callback onPageLoaded: e:${htmlobj.element}, p:${htmlobj.name}, i:${htmlobj.includes}`);
 
     TAB_CONTAINER.innerHTML= "";
     TAB_CONTAINER.append(htmlobj.element);
@@ -56,9 +72,31 @@
       b.includes= htmlobj.includes?? undefined;
       for(const i of b.includes) document.body.append(i);
     }
+
+    if(deviceDesktop) {
+      WORDS_PANNERS= document.getElementsByClassName("wordspanner");
+      bANY_PANNER= WORDS_PANNERS && WORDS_PANNERS.length > 0;
+      updatePanners();
+    }
   }
 
   function onPageNotFound(){ window.location.assign(`${window.location.origin}/404`); };
+
+  if(deviceDesktop){
+    function updatePanners(){
+      if(bANY_PANNER){
+        for(const e of WORDS_PANNERS) {
+          let box = e.getBoundingClientRect();
+          e.style.transform = `translate(0, ${box.y*.03}%)`;
+        };
+      }
+    }
+
+    let u= true;
+    document.addEventListener("scroll", (e) => {
+      if(u=!u) window.requestAnimationFrame(function () { updatePanners(); });
+    });
+  }
 
   parseCurrentUrl();
 }();
